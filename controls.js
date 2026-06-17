90382188d49def8dc858fb0d72a2ea98bfc976e3
tsubasa-x-dev <fujisan.283283283@gmail.com>
2026-06-17 11:57:12 +0900

    Add files via upload

---
+++ controls.js
@@ -0,0 +1,13 @@
+
+//停止ボタン//
+const stopButton = document.getElementById('stop-button');
+const clearButton = document.getElementById('clear-button');
+
+stopButton.addEventListener('click', () => {
+    speechSynthesis.cancel();
+});
+//リセットボタン//
+clearButton.addEventListener('click', () => {
+    const textInput = document.getElementById('text-input');
+    textInput.value = '';
+});
